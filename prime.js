ddf4ca8df2ce289840213c4cf3fc11490e16f9d8
satyamyadav <98868242+satyamy846@users.noreply.github.com>
2022-12-08 00:31:36 +0530

    Update prime.js

--- prime.js
+++ prime.js
@@ -1,5 +1,11 @@
 //Q. Given a number x, find out if it is a prime number or not, use javascript and find out the difference between Next prime number after x and x
-let num =17;
+
+//Logic is to first check if that given number is prime or not
+//Step 1. Create a function to check if it is a prime or not
+//Step 2. Create a another function start traversing from n+1 (where n is given number) until the next prime number we get.
+//Step 3. As we get next prime number return difference of that number and given number.
+
+let num = 17;
 //it will check the number is prime or not
 function checkPrime(num){
     for(let i=2;i<=Math.sqrt(num);i++){
